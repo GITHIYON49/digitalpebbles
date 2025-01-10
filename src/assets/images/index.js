9a92b309afd0310aed0346cce669b1b1a518c181
Githiyon49 <githiyon0409@gmail.com>
2025-01-10 10:16:07 +0530

    remove and convert image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,29 +1,27 @@
 import digitalpebbles from "../images/digitalpebbles.webp";
-import digitalpebbles1 from "../images/digitalpebbles1.svg";
-import slider1 from "./carosuel/webdevelopment.png";
-import slider2 from "./carosuel/appdevelopment.png";
-import slider3 from "./carosuel/ecommerce.png";
-import slider4 from "./carosuel/gadget.png";
-import slider5 from "./carosuel/traning_development.png";
-import whyus from "../images/why1_enhanced.png";
-import aboutbanner from "../images/slider1.svg";
-import aboutus from "../images/about1.jpg";
-import servicebanner from "./services/services.png";
-import contactbanner from "../images/contact_page.png";
-import webservices from "./services/web_services.png";
-import appservices from "./services/app_services.png";
-import ecommerceservices from "./services/ecommerce_services.png";
-import gadgetservices from "./services/gadget_services.png";
-import traingdevelopmentservices from "./services/trainig_development_services.png";
-import webdevelopment from "./services/webdevelopment.png";
-import appdevelopment from "./services/appdevelopment.png";
-import ecommerce from "./services/ecommerce.png";
-import gadgets from "./services/gadgets.png";
-import traningdevelopment from "./services/traing_development.png";
+import whyus from "../images/why1_enhanced.webp";
+import aboutbanner from "../images/slider1.webp";
+import aboutus from "../images/about1.webp";
+import contactbanner from "../images/contact_page.webp";
+import slider1 from "./carosuel/webdevelopment.webp";
+import slider2 from "./carosuel/appdevelopment.webp";
+import slider3 from "./carosuel/ecommerce.webp";
+import slider4 from "./carosuel/gadget.webp";
+import slider5 from "./carosuel/traning_development.webp";
+import servicebanner from "./services/services.webp";
+import webservices from "./services/web_services.webp";
+import appservices from "./services/app_services.webp";
+import ecommerceservices from "./services/ecommerce_services.webp";
+import gadgetservices from "./services/gadget_services.webp";
+import traingdevelopmentservices from "./services/trainig_development_services.webp";
+import webdevelopment from "./services/webdevelopment.webp";
+import appdevelopment from "./services/appdevelopment.webp";
+import ecommerce from "./services/ecommerce.webp";
+import gadgets from "./services/gadgets.webp";
+import traningdevelopment from "./services/traing_development.webp";
 
 export {
   digitalpebbles,
-  digitalpebbles1,
   slider1,
   slider2,
   slider3,
